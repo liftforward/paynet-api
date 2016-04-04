579bb4c75f10b7be4a91ab812ce3dc7dfa0ef297
Drew Nichols <drew@liftforward.com>
2016-04-04 16:32:45 -0400

    refactored client spec

--- lib/paynet_api/client.rb
+++ lib/paynet_api/client.rb
@@ -11,16 +11,16 @@ module PaynetApi
     def search_for_company(params)
       request = PaynetApi::SearchForCompany::Request.new(params)
 
-      response = get(request.url)
+      response = get(request)
 
       PaynetApi::SearchForCompany::Response.new(response_xml: response.body, request: request)
     end
 
 
     private
-      def get url
+      def get request
 
-        connection = Faraday.new url do |conn|
+        connection = Faraday.new request.url do |conn|
           # conn.response :xml,  :content_type => /\bxml$/
           conn.adapter Faraday.default_adapter
         end
