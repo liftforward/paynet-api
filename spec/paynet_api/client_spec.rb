35c2da2e0de5ffd5b7e0321476e8cd73310db240
Andrew Callahan <andrew.callahan@gmail.com>
2016-04-05 16:51:33 -0400

    extend client to cobrand pdf reports

--- spec/paynet_api/client_spec.rb
+++ spec/paynet_api/client_spec.rb
@@ -1,42 +1,10 @@
 describe PaynetApi::Client do
-  subject { PaynetApi::Client.new }
+  let(:client) { PaynetApi::Client.new }
 
-  # describe "#get_payment_history_report" do
-  #   let(:params) { { company_name: "Acme Co", city: "Durham", state_code: "NC"} }
-  #   let(:request_double) { instance_double(PaynetApi::GetPaymentHistoryReport::Request, :send! => "faraday object") }
-
-  #   context "with required params passed in" do
-  #     before do
-  #       allow(PaynetApi::GetPaymentHistoryReport::Request).to receive(:new).with(params).and_return(request_double)
-  #       allow(PaynetApi::GetPaymentHistoryReport::Response).to receive(:new).with(response: "faraday object").and_return("final response object")
-  #     end
-
-  #     it "should make a new paynet search request object with the passed in params" do
-  #       expect(PaynetApi::GetPaymentHistoryReport::Request).to receive(:new).with(params).and_return(request_double)
-  #       subject.get_payment_history_report(params)
-  #     end
-
-  #     it "should send the newly created request object to Paynet" do
-  #       expect(request_double).to receive(:send!)
-  #       subject.get_payment_history_report(params)
-  #     end
-
-  #     it "should make a new paynet search reponse object with the Paynet faraday response" do
-  #       expect(PaynetApi::GetPaymentHistoryReport::Response).to receive(:new).with(response: "faraday object")
-  #       subject.get_payment_history_report(params)
-  #     end
-
-  #     it "should return the final response object" do
-  #       allow(PaynetApi::GetPaymentHistoryReport::Response).to receive(:new).with(response: "faraday object").and_return("final response object")
-  #       expect(subject.get_payment_history_report(params)).to eq("final response object")
-  #     end
-  #   end
-  # end
-
-  describe "#search_for_company" do
-    let(:params) { { company_name: "Acme Co", city: "Durham", state_code: "NC"} }
-    let(:client) { PaynetApi::Client.new }
-    subject(:search) { client.search_for_company(params) }
+  describe "#cobrand_get_payment_history_report" do
+    let(:test_id_from_paynet) { "59206906" }
+    let(:params) { { paynet_id: test_id_from_paynet, payment_comprehensive: "1" } }
+    subject(:search) { client.cobrand_get_payment_history_report(params) }
 
     context "with required params passed in" do
       let(:faraday_response_double) { double("Response") }
@@ -45,10 +13,57 @@ describe PaynetApi::Client do
         expect(faraday_response_double).to receive(:body).and_return("<response></response>")
       end
 
-      it "should make a new paynet search for company request & response objects with the passed in params" do
+      it "should make new PaynetApi::CobrandGetPaymentHistoryReport Request & Response objects with the passed in params" do
+        expect(PaynetApi::CobrandGetPaymentHistoryReport::Request).to receive(:new).with(params)
+        expect(PaynetApi::CobrandGetPaymentHistoryReport::Response).to receive(:new)
+        subject
+      end
+
+      it "should return a PaynetApi::CobrandGetPaymentHistoryReport::Response" do
+        expect(subject).to be_a(PaynetApi::CobrandGetPaymentHistoryReport::Response)
+      end
+    end
+  end
+
+  describe "#get_payment_history_report" do
+    let(:test_id_from_paynet) { "59206906" }
+    let(:params) { { paynet_id: test_id_from_paynet, payment_comprehensive: "1" } }
+    subject(:search) { client.get_payment_history_report(params) }
+
+    context "with required params passed in" do
+      let(:faraday_response_double) { double("Response") }
+      before do
+        expect(client).to receive(:get).and_return(faraday_response_double)
+        expect(faraday_response_double).to receive(:body).and_return("<response></response>")
+      end
+
+      it "should make new PaynetApi::GetPaymentHistoryReport Request & Response objects with the passed in params" do
+        expect(PaynetApi::GetPaymentHistoryReport::Request).to receive(:new).with(params)
+        expect(PaynetApi::GetPaymentHistoryReport::Response).to receive(:new)
+        subject
+      end
+
+      it "should return a PaynetApi::GetPaymentHistoryReport::Response" do
+        expect(subject).to be_a(PaynetApi::GetPaymentHistoryReport::Response)
+      end
+    end
+  end
+
+  describe "#search_for_company" do
+    let(:params) { { company_name: "Acme Co", city: "Durham", state_code: "NC"} }
+    subject { client.search_for_company(params) }
+
+    context "with required params passed in" do
+      let(:faraday_response_double) { double("Response") }
+      before do
+        expect(client).to receive(:get).and_return(faraday_response_double)
+        expect(faraday_response_double).to receive(:body).and_return("<response></response>")
+      end
+
+      it "should make new PaynetApi::SearchForCompany Request & Response objects with the passed in params" do
         expect(PaynetApi::SearchForCompany::Request).to receive(:new).with(params)
         expect(PaynetApi::SearchForCompany::Response).to receive(:new)
-        search
+        subject
       end
 
       it "should return a PaynetApi::SearchForCompany::Response" do
